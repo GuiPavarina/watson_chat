7f9fcf4f4d7b13685968b32cd4e136846739add8
Guilherme sa <guilhermedesaster@gmail.com>
2017-11-25 18:57:43 -0200

    New chat interface

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,6 +1,15 @@
 module.exports.chat = (req, res) => {
 	
 	let formData = req.body;
+	let value = '';
+
+	if(req.body.nickname){
+		value = req.body.nickname;
+		value = value.replace(/\s/g, '');
+		
+		if(value.length < 3)
+			req.body.nickname = value;
+	}
 
 	req.assert('nickname','Nickname must not be empty').notEmpty();
 	req.assert('nickname','Nickname must have between 3 and 15 characters').len(3,15);
